c47194870bd8924576989c2d9ee78f121eefb946
Choi Esther <esther4007@gmail.com>
2015-11-13 20:31:35 +0900

    스플래시 액티비티생성

--- app/src/main/java/com/example/photori/photoribook/MainActivity.java
+++ app/src/main/java/com/example/photori/photoribook/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.photori.photoribook;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences shpref=getSharedPreferences("myPref",0);
+        //앱 종류 여부에 상관하지 않고 값을 저장하고 싶을때
+       /* int count=shpref.getInt("Count",-100);
+        if(count==-100){
+            startActivity(new Intent(MainActivity.this, SplashActivity.class));
+            count=1;
+        }
+        else {
+            count++;
+        }SharedPreferences.Editor editor=shpref.edit();
+        editor.putInt("Count",count);
+        editor.commit();
+*/
         setContentView(R.layout.activity_main);
+
+
     }
 }
---
+++ app/src/main/java/com/example/photori/photoribook/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.photori.photoribook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+    int SPLASH_TIME=2000;
+
+    @Override //화면을 보여주기 위해서 쓰는 것
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },SPLASH_TIME);
+    }
+}
