3dec98e3f758804a0bb26a3c349d57a8f7dc39a6
Choi Esther <esther4007@gmail.com>
2015-11-13 21:41:21 +0900

    회원가입 액티비티, xml 생성

--- app/src/main/java/com/example/photori/photoribook/LoginActivity.java
+++ app/src/main/java/com/example/photori/photoribook/LoginActivity.java
@@ -1,13 +1,37 @@
 package com.example.photori.photoribook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText id_text;
+    EditText pass_text;
+    Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        id_text=(EditText)findViewById(R.id.id_text);
+        pass_text=(EditText)findViewById(R.id.pass_text);
+        login_button=(Button)findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id_text.getText().toString();
+                pass_text.getText().toString();
+                Toast.makeText(LoginActivity.this, "로그인 성공~", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/photori/photoribook/SignupActivity.java
@@ -0,0 +1,7 @@
+package com.example.photori.photoribook;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity{
+
+}
