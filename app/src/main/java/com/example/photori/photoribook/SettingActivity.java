7033cc440d73ab6a0a22d932f1184bc4eeb96f46
Choi Esther <esther4007@gmail.com>
2015-11-26 12:28:50 +0900

    setting xml, java 수정

--- app/src/main/java/com/example/photori/photoribook/SettingActivity.java
+++ app/src/main/java/com/example/photori/photoribook/SettingActivity.java
@@ -1,14 +1,27 @@
 package com.example.photori.photoribook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingActivity extends AppCompatActivity{
+    Button logout_button;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-    }
+
+        logout_button = (Button)findViewById(R.id.logout_button);
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }   //onCreate
 }
