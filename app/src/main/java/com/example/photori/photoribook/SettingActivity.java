f64d2ac957b8cfcbec32f132e171ffde2ecdda5c
songmho <songmho@gmail.com>
2015-11-26 12:37:27 +0900

    setting Activity  수정

--- app/src/main/java/com/example/photori/photoribook/SettingActivity.java
+++ app/src/main/java/com/example/photori/photoribook/SettingActivity.java
@@ -1,19 +1,41 @@
 package com.example.photori.photoribook;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SettingActivity extends AppCompatActivity{
     Button logout_button;
+    Toolbar toolbar;
+    TextView current;
+    TextView present;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("설정");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        current=(TextView)findViewById(R.id.current);
+        present=(TextView)findViewById(R.id.present);
+
+        try {
+            present.setText(getPackageManager().getPackageInfo(getPackageName(), 0).versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
         logout_button = (Button)findViewById(R.id.logout_button);
         logout_button.setOnClickListener(new View.OnClickListener() {
