431c4abe02adff6061394c3d7bfb43b8cb3155fe
Choi Esther <esther4007@gmail.com>
2015-11-27 22:45:59 +0900

    스플래쉬 애니메이션 추가

--- app/src/main/java/com/example/photori/photoribook/SplashActivity.java
+++ app/src/main/java/com/example/photori/photoribook/SplashActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
-    int SPLASH_TIME=2000;
+    ImageView logo;
+    int SPLASH_TIME=2500;
 
     @Override //화면을 보여주기 위해서 쓰는 것
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,9 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        logo = (ImageView)findViewById(R.id.logo);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
+        logo.startAnimation(anim);
 
         new android.os.Handler().postDelayed(new Runnable() {
             @Override
